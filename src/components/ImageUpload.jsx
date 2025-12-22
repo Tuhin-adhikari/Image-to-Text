ebe3e42d0e967a9a537a5ef624df919dd387fb28
Tuhin-adhikari <tuhinworkspace2817@gmail.com>
2025-12-22 18:10:58 +0530

    route for backend changed

--- src/components/ImageUpload.jsx
+++ src/components/ImageUpload.jsx
@@ -35,7 +35,7 @@ export default function ImageUpload({ onResult }) {
             formData.append("preprocess_mode", mode);
 
             const res = await axios.post(
-                "https://img-to-txt-backend-production.up.railway.app/ocr",
+                "https://img-to-txt-pl0v.onrender.com/ocr",
                 formData,
                 {
                     headers: {
